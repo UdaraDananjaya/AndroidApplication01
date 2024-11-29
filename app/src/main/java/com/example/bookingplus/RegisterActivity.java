b34d48154082772a7127bb58715a7a95aa333f83
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-29 23:59:48 +0530

    main - Register Page Variable Naming Fix

--- app/src/main/java/com/example/bookingplus/RegisterActivity.java
+++ app/src/main/java/com/example/bookingplus/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.bookingplus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private AuthManager authManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,18 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        authManager = new AuthManager(this);
+
+        EditText emailEditText = findViewById(R.id.etUsername);
+        EditText passwordEditText = findViewById(R.id.etPassword);
+        Button signUpButton = findViewById(R.id.btnSignUp);
+
+        signUpButton.setOnClickListener(view -> {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            authManager.createUser(email, password);
+        });
+
         TextView loginLinkTextView = findViewById(R.id.tvLogin);
         loginLinkTextView.setOnClickListener(view -> {
             Intent intent = new Intent(this, LoginActivity.class);
