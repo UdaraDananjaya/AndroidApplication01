60ccc370bab0bbe25cd86bff734abc1204b89355
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-30 00:11:36 +0530

    main - Add Dashboard Buttons

--- app/src/main/java/com/example/bookingplus/MainActivity.java
+++ app/src/main/java/com/example/bookingplus/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bookingplus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnAddVehicle = findViewById(R.id.btnAddVehicle);
+        Button btnSearchVehicle = findViewById(R.id.btnSearchVehicle);
+        Button btnViewBookings = findViewById(R.id.btnViewBookings);
+
+        btnAddVehicle.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddVehicleActivity.class);
+            startActivity(intent);
+        });
+
+        btnSearchVehicle.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SearchVehicleActivity.class);
+            startActivity(intent);
+        });
+
+        btnViewBookings.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ViewBookingsActivity.class);
+            startActivity(intent);
         });
     }
 }
