43afdca3514d6db0ba70ae4fff57d86a64be6585
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-29 22:47:34 +0530

    main - AuthManager Method adding

--- app/src/main/java/com/example/bookingplus/AuthManager.java
+++ app/src/main/java/com/example/bookingplus/AuthManager.java
@@ -1,4 +1,54 @@
 package com.example.bookingplus;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class AuthManager {
+    private final FirebaseAuth firebaseAuth;
+    private final Context context;
+
+    public AuthManager(Context context) {
+        this.context = context;
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void createUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // FirebaseUser user = firebaseAuth.getCurrentUser();
+                        Toast.makeText(context, "Registration Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(context, LoginActivity.class);
+                        context.startActivity(intent);
+                    } else {
+                        Toast.makeText(context, "Failed: " + (task.getException() != null ? task.getException().getMessage() : "Unknown error"), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void loginUser(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(context, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(context, MainActivity.class);
+                        context.startActivity(intent);
+
+                    }else{
+                        Toast.makeText(context, "Login Failed" + (task.getException() != null ? task.getException().getMessage() : "Unknown error"), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public void signOut() {
+        firebaseAuth.signOut();
+        Toast.makeText(context, "Logout Successfully", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(context, LoginActivity.class);
+        context.startActivity(intent);
+    }
+
 }
