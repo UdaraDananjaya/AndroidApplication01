fa5661612050ba8bed4925ff45f90bec9191b509
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-29 23:54:14 +0530

    main - Register Page Functions

--- app/src/main/java/com/example/bookingplus/RegisterActivity.java
+++ app/src/main/java/com/example/bookingplus/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.bookingplus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,11 @@ public class RegisterActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TextView loginLinkTextView = findViewById(R.id.tvLogin);
+        loginLinkTextView.setOnClickListener(view -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
